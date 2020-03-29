31488080d55e3256bf7d5987d8c061be021f4135
Andrew Davisson <akdavisson4@gmail.com>
2020-03-28 22:57:11 -0600

    added react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import '../styles/App.css';
+import { Switch, Route } from 'react-router-dom';
 import LinkList from './LinkList';
 import CreateLink from './CreateLink';
+import Header from './Header';
 
 function App() {
   return (
-    <div className="App">
-      <CreateLink />
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+        </Switch>
+      </div>
     </div>
   );
 }

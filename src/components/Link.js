7b10083b0c69e346205106f132d52b14b8860a5f
Andrew Davisson <akdavisson4@gmail.com>
2020-03-29 09:18:16 -0600

    updated for voting

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { AUTH_TOKEN } from '../constants';
 
 const Link = (props) => {
+  const authToken = localStorage.getItem(AUTH_TOKEN)
+
   return (
-    <div>
-      <div>
-        {props.link.description} ({props.link.url})
+    <div className="flex mt2 items-start">
+      <div className="flex items-center">
+        <span className="gray">{props.index + 1}.</span>
+        {authToken && (
+          <div className="ml1 gray f11" onClick={() => _voteForLink()}>
+            ▲
+          </div>
+        )}
+      </div>
+      <div className="ml1">
+        <div>
+          {this.props.link.description} ({props.link.url})
+        </div>
+        <div className="f6 lh-copy gray">
+          {this.props.link.votes.length} votes | by{' '}
+          {this.props.link.postedBy
+            ? this.props.link.postedBy.name
+            : 'Unknown'}{' '}
+          {timeDifferenceForDate(props.link.createdAt)}
+        </div>
       </div>
     </div>
   );
